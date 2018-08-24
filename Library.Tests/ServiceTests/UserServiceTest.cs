341041c3855399c24c911adb78669b77374b767b
vinayb <vinayb@VINAYKUMAR>
2018-08-24 10:55:38 +0530

    Basic unit test

--- Library.Tests/ServiceTests/UserServiceTest.cs
+++ Library.Tests/ServiceTests/UserServiceTest.cs
@@ -7,25 +7,83 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Unity;
+using Library.BusinessObjects;
+using Library.BusinessObjects.enums;
 
 namespace Library.Tests.ServiceTests
 {
     [TestClass]
     public class UserServiceTest
     {
-        [TestMethod]
-        public void validGetUser()
+
+        IUserService userService;
+        [TestInitialize]
+        public void Initialize()
         {
             UnityContainer unityContainer = new UnityContainer();
             unityContainer.RegisterType<IUserService, UserService>();
             unityContainer.RegisterType<IUserRepository, UserRepository>();
-            IUserService userService = unityContainer.Resolve<UserService>();
+            userService = unityContainer.Resolve<UserService>();
+        }
 
-
+        [TestMethod]
+        public void GetValidUser()
+        {            
             var data = userService.GetUserById(8);
             Assert.IsTrue(data.username == "vinay" );
-
         }
-        
+        [TestMethod]
+        public void GetInvalidUser()
+        {
+            var data = userService.GetUserById(4545);
+            Assert.IsNull(data);
+        }
+        [TestMethod]
+        public void AddUserStudent()
+        {
+            User newUser = new User();
+            newUser.name = "UnitTest";
+            newUser.password = "password";
+            newUser.roleName = UserType.Student;
+            newUser.username = "unitTest";
+            newUser.address = "westernpearl";
+            Assert.IsTrue(userService.AddUser(newUser));
+        }
+        [TestMethod]
+        public void AddUserStaff()
+        {
+            User newUser = new User();
+            newUser.name = "UnitTest";
+            newUser.password = "password";
+            newUser.roleName = UserType.Staff;
+            newUser.username = "unitTest";
+            newUser.address = "westernpearl";
+            bool newUserCreated = userService.AddUser(newUser);
+            Assert.IsTrue(newUserCreated);
+        }
+        [TestMethod]
+        public void AddUserLibrarian()
+        {
+            User newUser = new User();
+            newUser.name = "UnitTest";
+            newUser.password = "password";
+            newUser.roleName = UserType.Librarian;
+            newUser.username = "unitTest";
+            newUser.address = "westernpearl";
+            bool newUserCreated = userService.AddUser(newUser);
+            Assert.IsTrue(newUserCreated);
+        }
+        [TestMethod]
+        public void RemoveValidUser()
+        {
+            Assert.IsTrue(userService.RemoveUser(10));
+        }
+        [TestMethod]
+        public void RemoveInValidUser()
+        {
+            Assert.IsTrue(userService.RemoveUser(1022));
+        }
+
+
     }
 }
